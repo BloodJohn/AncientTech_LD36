e07c00b20ca2043bcf9f3a437f802596a97dc325
Eugene Pavlov <bjohn.ext@gmail.com>
2016-08-27 14:55:48 +0300

    bugfix fish

---
+++ Assets/Script/SummerController.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SummerController : MonoBehaviour
+{
+    public const int peopleCount = 12;
+    public const string sheepCountKey = "sheep";
+    public const string haylageCountKey = "haylage";
+    public const string fishCountKey = "fish";
+
+    /// <summary>Сколько дней осталось</summary>
+    public Text title;
+    /// <summary>Сколько овец</summary>
+    public Text sheepLabel;
+    /// <summary>сколько сена(лугов)</summary>
+    public Text hayLabel;
+    /// <summary>сколько рыбы</summary>
+    public Text fishLabel;
+
+    public Button haylageButton;
+    public Button fishingButton;
+    public Button longhouseButton;
+
+    /// <summary>дней</summary>
+    public int dayCount;
+    /// <summary>овец</summary>
+    public int sheepCount;
+    /// <summary>лугов</summary>
+    public int landCount;
+    /// <summary>сена</summary>
+    public int haylageCount;
+    /// <summary>трески</summary>
+    public int fishCount;
+    /// <summary>рыбы в море</summary>
+    public int seaCount;
+
+    public void Awake()
+    {
+        sheepCount = PlayerPrefs.GetInt(sheepCountKey, 12);
+
+        ShowStats();
+    }
+
+    public void ShowStats()
+    {
+        title.text = string.Format("Summer {0}", dayCount);
+        sheepLabel.text = string.Format("Sheeps {0}/{1} land", sheepCount, landCount);
+        hayLabel.text = string.Format("Haylage {0}/{1} land", haylageCount, landCount);
+        fishLabel.text = string.Format("Сodfish {0}/{1} sea", fishCount, seaCount);
+
+        var isSummer = dayCount > 0;
+
+        haylageButton.gameObject.SetActive(isSummer && landCount > sheepCount);
+        fishingButton.gameObject.SetActive(isSummer && seaCount > 0);
+        longhouseButton.gameObject.SetActive(!isSummer);
+    }
+
+    public void DayClick()
+    {
+        if (landCount < sheepCount) sheepCount = landCount;
+        landCount -= sheepCount;
+        dayCount--;
+    }
+
+    public void HaylageClick()
+    {
+        DayClick();
+
+        var production = peopleCount * 2;
+        if (production > landCount) production = landCount;
+
+        haylageCount += production;
+        landCount -= production;
+
+        ShowStats();
+    }
+
+    public void FishingClick()
+    {
+        DayClick();
+
+        var production = seaCount / 1000;
+        if (production > seaCount) production = seaCount;
+
+        fishCount += production;
+        seaCount -= production;
+        ShowStats();
+    }
+
+    public void WinterClick()
+    {
+        PlayerPrefs.SetInt(sheepCountKey,sheepCount);
+        PlayerPrefs.SetInt(haylageCountKey, haylageCount);
+        PlayerPrefs.SetInt(fishCountKey, fishCount);
+        SceneManager.LoadScene(1);
+    }
+
+    public void RestartClick()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Script/WinterController.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinterController : MonoBehaviour
+{
+    public const string feltedCountKey = "felted";
+
+    /// <summary>Сколько дней осталось</summary>
+    public Text title;
+    /// <summary>Сколько овец</summary>
+    public Text sheepLabel;
+    /// <summary>сколько мяса</summary>
+    public Text woolLabel;
+    /// <summary>сколько еды (мяса и рыбы)</summary>
+    public Text foodLabel;
+
+    public Button slaughterButton;
+    public Button woolButton;
+    public Button idleButton;
+    public Button summerButton;
+
+    /// <summary>дней</summary>
+    public int dayCount;
+    /// <summary>овец</summary>
+    public int sheepCount;
+    /// <summary>сена</summary>
+    public int haylageCount;
+    /// <summary>шерсть</summary>
+    public int woolCount;
+    /// <summary>ткань</summary>
+    public int feltedCount;
+    /// <summary>мяса</summary>
+    public int meatCount;
+    /// <summary>трески</summary>
+    public int fishCount;
+
+    public void Awake()
+    {
+        woolCount = sheepCount;
+        feltedCount = PlayerPrefs.GetInt(feltedCountKey);
+        sheepCount = PlayerPrefs.GetInt(SummerController.sheepCountKey);
+        haylageCount = PlayerPrefs.GetInt(SummerController.haylageCountKey);
+        fishCount = PlayerPrefs.GetInt(SummerController.fishCountKey);
+        ShowStats();
+    }
+
+    public void ShowStats()
+    {
+        title.text = string.Format("Winter {0}", dayCount);
+        sheepLabel.text = string.Format("Sheeps {0} hay {1}", sheepCount, haylageCount);
+        woolLabel.text = string.Format("Felted {0}/{1} wool", feltedCount, woolCount);
+        foodLabel.text = string.Format("Meat {0}/{1} codfish", meatCount, fishCount);
+
+        var isWinter = dayCount > 0;
+
+
+        slaughterButton.gameObject.SetActive(isWinter && sheepCount > 0);
+        woolButton.gameObject.SetActive(isWinter && woolCount > 0);
+        idleButton.gameObject.SetActive(isWinter);
+        summerButton.gameObject.SetActive(!isWinter);
+    }
+
+    public void SlaughterClick()
+    {
+        sheepCount--;
+        meatCount++;
+        ShowStats();
+    }
+
+
+    public void WoolClick()
+    {
+        if (woolCount > 0)
+        {
+            woolCount--;
+            feltedCount++;
+        }
+
+        DayClick();
+    }
+
+    public void DayClick()
+    {
+        dayCount--;
+        //нет сена - забиваем скот
+        while (haylageCount < sheepCount && sheepCount > 0) SlaughterClick();
+        haylageCount -= sheepCount;
+
+        //кормим людей
+        if (meatCount > 0)
+            meatCount--;
+        else if (fishCount > 0)
+            fishCount--;
+        else
+        {
+            SceneManager.LoadScene(2);
+            return;
+        }
+
+        ShowStats();
+    }
+
+    public void SummerClick()
+    {
+        if (sheepCount > 1) sheepCount += sheepCount / 2;
+        PlayerPrefs.SetInt(SummerController.sheepCountKey, sheepCount);
+        PlayerPrefs.SetInt(feltedCountKey, feltedCount);
+
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestartClick()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(0);
+    }
+}
